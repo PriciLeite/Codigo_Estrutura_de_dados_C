5f0e2ccec151d06260b1b1640caab3bd48b05495
Priscila Leite <pricila135leite@gmail.com>
2023-01-23 21:35:39 -0300

    Pilhas - Verificação de alocação
    /*
     Verificações na pilha: 1) Verificando se a pilha está vazia...
    ° Push - Coloca uma informação na pilha (Empilha)
    ° Pop - Retira uma informação da Pilha (Desempilha)
    ° Size - Retorna o TAMANHO da pilha.
    ° StackPop - Retorna o elemento SUPERIOR sem removelo (equivalente a Pop e Push).
    ° Empty - Verifica se a pilha está vazia.
    */

---
+++ estrutura_de_dados_Operacoes_Na_Pilha.c
@@ -0,0 +1,36 @@
+/*
+ Verificações na pilha: 1) Verificando se a pilha está vazia... 
+° Push - Coloca uma informação na pilha (Empilha)
+° Pop - Retira uma informação da Pilha (Desempilha)
+° Size - Retorna o TAMANHO da pilha.
+° StackPop - Retorna o elemento SUPERIOR sem removelo (equivalente a Pop e Push).
+° Empty - Verifica se a pilha está vazia. 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(int argc, char const *argv[])
+{
+    char Variavel = 'abc123';
+
+    printf("\nDigite a variavel que deseja verificar: ");
+    return 1 || 0;
+
+   /*   begin
+            if topo variavel == 0 then
+                return true;
+
+            else                    // Verificação...
+                return false
+            endif
+        end     
+   */ 
+
+
+
+
+    return 0;
+}
+
